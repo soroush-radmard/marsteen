e09189f2ced5fb790a7fda541b4f15ea7a8df6df
Soroushiii <soroushradmard@gmail.com>
2022-08-06 17:25:12 +0430

    carousel added to customer section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,6 +8,10 @@ let navMenu = document.querySelector(".menu-items");
 let navMenuBtn = document.querySelector(".dropdown-menu");
 let navMenuBtnIcon = document.querySelector(".dropdown-menu i");
 
+let slidePosition = 0;
+const slides = document.getElementsByClassName('customer-info');
+const totalSlides = slides.length;
+
 const toggleAudio = (event) => {
 	if (isPlaying) {
 		audio.pause()
@@ -28,8 +32,6 @@ audio.onloadedmetadata = () => {
 
 //////////////////////////////////////////////////////////////
 
-
-
 navMenuBtn.addEventListener("click", function(){
     if(navMenuBtnIcon.classList.contains("fa-bars")){
         navMenu.style.left = "0";
@@ -41,3 +43,45 @@ navMenuBtn.addEventListener("click", function(){
         navMenuBtnIcon.classList = "fa fa-bars";
     }
 })
+
+//////////////////////////////////////////////////////////////
+
+document.
+  getElementById('next')
+  .addEventListener("click", function() {
+    moveToNextSlide();
+  });
+document.
+  getElementById('previous')
+  .addEventListener("click", function() {
+    moveToPrevSlide();
+  });
+
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('active-info');
+    // slide.classList.add('carousel__item--hidden');
+  }
+
+  slides[slidePosition].classList.add('active-info');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}
